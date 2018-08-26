b13b3a862ca3ba878ab8228e83400539f130704f
Brad Peabody <brad@peabody.io>
2018-08-26 12:29:26 -0700

    more prototyping

--- main.go
+++ main.go
@@ -75,6 +75,7 @@ func (ws *MainHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	// doing the webview.Bind() approach and hoping it handles complex
 	// data without issues (and still doesn't provide a way to
 	// push to the browser).
+
 	// TODO: we'll also need to secure the endpoint somehow, probably would
 	// work to generate a random key and Bind() that and require it
 	// in each request as a security precaution.  Doesn't prevent sniffing
